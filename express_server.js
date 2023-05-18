fa384476022765d3990a6196a74ae56735c04802
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-17 18:52:23 -0700

    installed morgan middleware package

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const morgan = require("morgan");
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
