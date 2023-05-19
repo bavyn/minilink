fe096b090483a63370d575700737a5e657ff0bf6
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-19 15:44:04 -0700

    redirect to login upon logout

--- express_server.js
+++ express_server.js
@@ -176,7 +176,7 @@ app.post("/login", (req, res) => {
 // logout
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 // ----- listen --------------------------------
