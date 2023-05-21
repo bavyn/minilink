c3d914d40015a8a0ca35b8b6db425c2b4ad3119e
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-20 17:21:01 -0700

    removed /urls.json and /hello

--- express_server.js
+++ express_server.js
@@ -29,15 +29,6 @@ app.get("/", (req, res) => {
     return res.redirect("/urls");
   }
   res.redirect("/login");
-  //res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
